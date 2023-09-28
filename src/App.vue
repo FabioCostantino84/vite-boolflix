c70518dff4672a3f5f2e17bca4b4d73d96d88173
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-28 15:02:21 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+import { state } from './state.js'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      state,
+    }
+  }
+}
+</script>
+
+
+
+<template>
+
+</template>
+
+
+
+<style lang="scss">
+@use './assets/app'
+</style>
