b58f91caa3c49aaeff2c04195fce7ba597eff2ca
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-28 15:52:23 +0200

    add input/button

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,11 @@ export default {
 
 
 <template>
-
+  <div class="container input-group mb-3">
+    <input type="text" class="form-control" placeholder="Research" aria-label="Research"
+      aria-describedby="button-addon2">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Cerca</button>
+  </div>
 </template>
 
 
