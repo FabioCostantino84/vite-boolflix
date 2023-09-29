40aad926d5c791d49831ef8622e40f48bbf8d83e
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-29 17:06:25 +0200

    add fixed methods and fetchMovie name

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,41 @@ export default {
     return {
       state,
     }
-  }
+  },
+  methods: {
+    searchFilmInfo() {
+      const urlMovie = this.state.urlMovieInfo + this.state.research;
+      let resultMovie = this.state.fetchMovieName(urlMovie)
+      //console.log(resultMovie)
+      // mi restituisce una stringa vuota nel mio input
+      this.searchFilmInfo = '';
+    },
+  },
 }
+
 </script>
 
 
 
 <template>
   <div class="container input-group mb-3">
-    <input type="text" class="form-control" placeholder="Research" aria-label="Research"
-      aria-describedby="button-addon2">
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Cerca</button>
+    <input type="search"  class="form-control" placeholder="Research" aria-label="Research" aria-describedby="button-addon2"
+      v-model="state.research">
+    <button class="btn btn-outline-secondary" @click="searchFilmInfo()" type="button" id="button-addon2">Cerca</button>
   </div>
+
+  <ul>
+    <h1>FILM</h1>
+    
+    <li v-for="movie in this.state.movies">
+      <div>TITOLO: {{ movie.title }} </div>
+      <div>TITOLO ORIGINALE: {{ movie.original_title }}</div>
+      <div>VOTO: {{ Math.floor(movie.vote_average)}}</div>
+      <div>LINGUA: {{ movie.original_language.toUpperCase() }}</div>
+    </li>
+  </ul>
 </template>
+
 
 
 
--- src/state.js
+++ src/state.js
@@ -1,26 +1,33 @@
 import { reactive } from "vue";
 import axios from 'axios';
 
-export const state = reactive ({
+export const state = reactive({
 
-    urlMovie: 'https://api.themoviedb.org/3/search/movie?api_key=f8440c94019485ae649439c217a34225&language=it_IT&query=',
-    
+    urlMovieInfo: 'https://api.themoviedb.org/3/search/movie?api_key=f8440c94019485ae649439c217a34225&language=it_IT&query=',
+
+    movies: [],
+    research: '',
+    original_language: '',
+
+    // mi permette di fare una chiamata a uno specifico indirizzo
+    // url serve per passare un imput generico (può avere un nome di fantasia me meglio richiamare argomento)
     fetchMovieName(url) {
 
-
+        // con questo metodo effetuo una chiamata , per prendre un dato
+        // passo lo stesso paramtro(url) il metodo è riutilizzabile
         axios
             .get(url)
-            .then(response => {
-
-                this.movies = response.data.results
-                console.log(response.data.results);
-
+            .then(res => {
+                console.log(res.data + 'ecco il response');
+                this.movies = res.data.results;
+                console.log(this.movies);
             })
-            .catch(error => {
-                console.error(error);
-            })
+
+        /* .catch(error => {
+            console.error(error);
+        }) */
 
     },
-    
+
 })
 
